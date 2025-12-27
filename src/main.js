5e06a1affb5204b6c1ef780649b7b3c842babc1c
Lucas Tonolli <lucastonolli973@gmail.com>
2025-12-27 11:13:37 -0300

    feat: configure `i18n`

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import { createApp } from 'vue'
 import './assets/css/base.css'
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
 
+import pt from './locales/pt.json'
+import en from './locales/en.json'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'pt',
+  fallbackLocale: 'en',
+  messages: {
+    pt,
+    en,
+  },
+})
 const app = createApp(App)
 
 app.use(router)
-
+app.use(i18n)
 app.mount('#app')

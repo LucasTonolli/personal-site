75e332e889d632b1fe32f1ca8f94a9756779161f
Lucas Tonolli <lucastonolli973@gmail.com>
2025-12-27 12:16:48 -0300

    refactor: remove `x` social link

--- src/components/home/HomeHero.vue
+++ src/components/home/HomeHero.vue
@@ -100,25 +100,6 @@ watch(locale, () => {
         </a>
 
         <a
-          href="https://x.com/Tonolli_Lucas"
-          target="_blank"
-          class="social-btn x-social"
-          title="X (Twitter)"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            height="22"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-          >
-            <path
-              d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-            />
-          </svg>
-        </a>
-
-        <a
           href="https://www.instagram.com/lucas_tonolli97/"
           target="_blank"
           class="social-btn instagram"
